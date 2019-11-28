0243e220b02e8adb483632e39f1dbea232d70986
Charles Fritz <cfritz@broomfield.org>
2019-11-27 18:46:16 -0700

    added pubsub script for sensor data publishing

--- bme_sensor_pubsub.py
+++ bme_sensor_pubsub.py
@@ -3,11 +3,7 @@ import smbus2
 from time import sleep
 from json import dumps
 import json
-from datetime import date, datetime
-import os
-import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import db
+from google.cloud import pubsub
 
 port = 1
 address = 0x76
@@ -17,46 +13,29 @@ bme280.load_calibration_params(bus, address)
 
 
 def main():
-    cred = credentials.Certificate(os.getenv('GOOGLE_APPLICATION_CREDENTIALS'))
-    url = os.getenv('FIREBASE_DB_URL')
+    project_id = "home-weather-iot-firebase"
+    topic_name = "device-signals"
 
-    db = firebase_creds(cred, url)
+    publisher = pubsub.PublisherClient()
+    topic_path = publisher.topic_path(project_id, topic_name)
 
     # # with open('data.json', 'r') as json_file:
     # #     json_data = json.load(json_file)
     #
-    # post_firebase(bme280_db, json_data)
-
 
     data = {}
     data['reading'] = []
+    sensor_data = read_data(bus, address, data, publisher, topic_path)
+
+def publish_message(data, publisher, topic_path):
+    # Data must be a bytestring
+    data_encode = data.encode('utf-8')
+    # When you publish a message, the client returns a future.
+    future = publisher.publish(topic_path, data=data_encode)
+    print(future.result())
 
 
-def firebase_creds(credentials, firebase_db_url):
-    # Initialize the app with a custom auth variable, limiting the server's access
-    app = firebase_admin.initialize_app(credentials, {
-
-        'databaseURL': firebase_db_url
-
-    })
-    sensor_db = db.reference()
-
-    return sensor_db
-
-def post_firebase(db, json_data):
-    data = db.child('bme280').push(json_data)
-    print("data inserted")
-    return data
-
-def json_serial(obj):
-    """JSON serializer for objects not serializable by default json code"""
-
-    if isinstance(obj, (datetime, date)):
-        return obj.isoformat()
-    raise TypeError ("Type %s not serializable" % type(obj))
-
-
-def read_data(bus, address, data, db):
+def read_data(bus, address, data, publisher, topic_path):
     while True:
         bme280_data = bme280.sample(bus, address)
         id = bme280_data.id
@@ -77,7 +56,8 @@ def read_data(bus, address, data, db):
         print(data)
         sleep(10)
 
-        post_firebase(data, db)
+        publish_message(data, publisher, topic_path)
+
 
         # with open('data.json', 'w') as json_file:
         #     json.dump(data, json_file)
